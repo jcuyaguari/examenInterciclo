6bece62030a40f2969869c6f79c2e0ca57d9ba14
Andres Loja <aloja619@gmail.com>
2019-05-29 22:57:29 -0500

    Pedido

---
+++ privada/pedidos/metodos.js
@@ -0,0 +1,48 @@
+function cambiar(opc) {
+    switch (opc) {
+        case '1':
+            var element = document.getElementById('1');
+            element.style.display = 'block';
+            var element1 = document.getElementById('2');
+            element1.style.display = 'none';
+            var element2 = document.getElementById('3');
+            element2.style.display = 'none';
+            var element2 = document.getElementById('4');
+            element2.style.display = 'none';
+            break;
+        case '2':
+            var element = document.getElementById('1');
+            element.style.display = 'none';
+            var element1 = document.getElementById('2');
+            element1.style.display = 'block';
+            var element2 = document.getElementById('3');
+            element2.style.display = 'none';
+            var element2 = document.getElementById('4');
+            element2.style.display = 'none';
+            break;
+        case '3':
+            var element = document.getElementById('1');
+            element.style.display = 'none';
+            var element1 = document.getElementById('2');
+            element1.style.display = 'none';
+            var element2 = document.getElementById('3');
+            element2.style.display = 'block';
+            var element2 = document.getElementById('4');
+            element2.style.display = 'none';
+            break;
+            case '4':
+            var element = document.getElementById('1');
+            element.style.display = 'none';
+            var element1 = document.getElementById('2');
+            element1.style.display = 'none';
+            var element2 = document.getElementById('3');
+            element2.style.display = 'none';
+            var element2 = document.getElementById('4');
+            element2.style.display = 'block';
+            break;
+    }
+}
+
+function crearlocal(){
+    
+}
