26769ddb57f921e663b357660d2c31193823c859
RIcardoVinicioJaraJara <49212955+RIcardoVinicioJaraJara@users.noreply.github.com>
2019-05-29 23:23:35 -0500

    Productos Metodo Crear

--- privada/productos/metodos.php
+++ privada/productos/metodos.php
@@ -16,14 +16,18 @@
             $precio = ($_GET['precio']);
             $stock = ($_GET['stock']);
             $img = ($_GET['img']);
-            $image = new Imagick();
-            $image->readimageblob($img);
-            echo '<img src="data:image/png;base64,' .  base64_encode($image->getimageblob())  . '" />';
-            echo "<p>$nombre </p>";
-            echo "<p>$tipo </p>";
-            echo "<p>$precio </p>";
-            echo "<p>$stock ddd </p>";
-            echo "<p>$img </p>";
+
+            $sql = "INSERT INTO productos VALUES (0, '$nombre', '$tipo', '$precio', '$stock', '$img',0)";
+            if ($conn->query($sql) === TRUE) {
+                echo "TRUE";
+            } else {
+                if ($conn->errno == 1062) {
+                    echo "<p class='error'>La persona con la cedula $cedula ya esta registrada en el sistema </p>";
+                } else {
+                    echo "<p class='error'>Error: " . mysqli_error($conn) . "</p>";
+                }
+            }
+            //cerrar la base de datos
             break;
         default:
             # code...
