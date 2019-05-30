8c6fa89e80e0463fe46e03c6c5a29642d020b7b7
jcuyaguari <juan_nicoc@hotmail.com>
2019-05-29 19:12:11 -0500

    interfaz usuario

---
+++ publica/menuP/cambiar.js
@@ -0,0 +1,45 @@
+function cambiar(opc) {
+    switch (opc) {
+        case '1':
+            var element = document.getElementById('1');
+            element.style.display = 'none';
+            var element1 = document.getElementById('2');
+            element1.style.display = 'block';
+            var element2 = document.getElementById('3');
+            element2.style.display = 'none';
+            var element2 = document.getElementById('4');
+            element2.style.display = 'none';
+            break;
+        case '2':
+            var element = document.getElementById('1');
+            element.style.display = 'none';
+            var element1 = document.getElementById('3');
+            element1.style.display = 'block';
+            var element2 = document.getElementById('2');
+            element2.style.display = 'none';
+            var element2 = document.getElementById('4');
+            element2.style.display = 'none';
+            break;
+
+        case '3':
+            var element = document.getElementById('2');
+            element.style.display = 'none';
+            var element1 = document.getElementById('1');
+            element1.style.display = 'block';
+            var element2 = document.getElementById('3');
+            element2.style.display = 'none';
+            var element2 = document.getElementById('4');
+            element2.style.display = 'none';
+            break;
+        case '4':
+            var element = document.getElementById('2');
+            element.style.display = 'none';
+            var element1 = document.getElementById('1');
+            element1.style.display = 'none';
+            var element2 = document.getElementById('3');
+            element2.style.display = 'none';
+            var element2 = document.getElementById('4');
+            element2.style.display = 'block';
+            break;
+    }
+}
