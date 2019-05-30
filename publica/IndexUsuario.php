0fb0ce818e6c8b9868df775f3f138bb32622580f
jcuyaguari <juan_nicoc@hotmail.com>
2019-05-30 12:29:53 -0500

    add

--- publica/IndexUsuario.php
+++ publica/IndexUsuario.php
@@ -16,10 +16,17 @@ if (!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE) {
 </head>
 
 <body onload="cambiar('1')">
-
+    <?php
+    include '../../../config/conexionBD.php';
+    ?>
+    <ul class="menu">
+        <li><a href="#" onclick="cambiar('1')" title="Enlace genérico">CORREOS</a></li>
+        <li><a href="#" onclick="cambiar('2')" title="Enlace genérico">NUEVO CORREO</a></li>
+        <li><a href="#" onclick="cambiar('3')" title="Enlace genérico">MI CUENTA</a></li>
+        <li><a href="#" onclick="cambiar('4')" title="Enlace genérico">BUSCAR CORREO</a></li>
+    </ul>
     <?php
     $codigo = $_GET["codigo"];
     $sql = "SELECT * FROM usuario where usu_codigo=$codigo";
     $row = $conn->query($sql)->fetch_assoc();
-
     ?>
