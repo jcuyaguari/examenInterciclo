06b1d37d0845e8ace54009156ad851244c64fc86
Andres Loja <aloja619@gmail.com>
2019-06-02 22:53:36 -0500

    cambiar

--- publica/menuP/cambiar.js
+++ publica/menuP/cambiar.js
@@ -2,6 +2,22 @@ function cambiar(opc) {
     switch (opc) {
         case '1':
             var element = document.getElementById('1');
+            element.style.display = 'block';
+            var element1 = document.getElementById('2');
+            element1.style.display = 'none';
+            var element2 = document.getElementById('3');
+            element2.style.display = 'none';
+            var element2 = document.getElementById('4');
+            element2.style.display = 'none';
+            var element2 = document.getElementById('5');
+            element2.style.display = 'none'
+            var element2 = document.getElementById('6');
+            element2.style.display = 'none'
+            var element2 = document.getElementById('7');
+            element2.style.display = 'none'
+            break;
+        case '2':
+            var element = document.getElementById('1');
             element.style.display = 'none';
             var element1 = document.getElementById('2');
             element1.style.display = 'block';
@@ -9,37 +25,93 @@ function cambiar(opc) {
             element2.style.display = 'none';
             var element2 = document.getElementById('4');
             element2.style.display = 'none';
-            break;
-        case '2':
-            var element = document.getElementById('1');
-            element.style.display = 'none';
-            var element1 = document.getElementById('3');
-            element1.style.display = 'block';
-            var element2 = document.getElementById('2');
-            element2.style.display = 'none';
-            var element2 = document.getElementById('4');
-            element2.style.display = 'none';
+            var element2 = document.getElementById('5');
+            element2.style.display = 'none'
+            var element2 = document.getElementById('6');
+            element2.style.display = 'none'
+            var element2 = document.getElementById('7');
+            element2.style.display = 'none'
             break;
 
         case '3':
-            var element = document.getElementById('2');
+            var element = document.getElementById('1');
             element.style.display = 'none';
-            var element1 = document.getElementById('1');
-            element1.style.display = 'block';
+            var element1 = document.getElementById('2');
+            element1.style.display = 'none';
             var element2 = document.getElementById('3');
-            element2.style.display = 'none';
+            element2.style.display = 'block';
             var element2 = document.getElementById('4');
             element2.style.display = 'none';
+            var element2 = document.getElementById('5');
+            element2.style.display = 'none'
+            var element2 = document.getElementById('6');
+            element2.style.display = 'none'
+            var element2 = document.getElementById('7');
+            element2.style.display = 'none'
             break;
         case '4':
-            var element = document.getElementById('2');
+            var element = document.getElementById('1');
             element.style.display = 'none';
-            var element1 = document.getElementById('1');
+            var element1 = document.getElementById('2');
             element1.style.display = 'none';
             var element2 = document.getElementById('3');
             element2.style.display = 'none';
             var element2 = document.getElementById('4');
             element2.style.display = 'block';
+            var element2 = document.getElementById('5');
+            element2.style.display = 'none'
+            var element2 = document.getElementById('6');
+            element2.style.display = 'none'
+            var element2 = document.getElementById('7');
+            element2.style.display = 'none'
+            break;
+        case '5':
+            var element = document.getElementById('1');
+            element.style.display = 'none';
+            var element1 = document.getElementById('2');
+            element1.style.display = 'none';
+            var element2 = document.getElementById('3');
+            element2.style.display = 'none';
+            var element2 = document.getElementById('4');
+            element2.style.display = 'none';
+            var element2 = document.getElementById('5');
+            element2.style.display = 'block'
+            var element2 = document.getElementById('6');
+            element2.style.display = 'none'
+            var element2 = document.getElementById('7');
+            element2.style.display = 'none'
+            break;
+        case '6':
+            var element = document.getElementById('1');
+            element.style.display = 'none';
+            var element1 = document.getElementById('2');
+            element1.style.display = 'none';
+            var element2 = document.getElementById('3');
+            element2.style.display = 'none';
+            var element2 = document.getElementById('4');
+            element2.style.display = 'none';
+            var element2 = document.getElementById('5');
+            element2.style.display = 'none'
+            var element2 = document.getElementById('6');
+            element2.style.display = 'block'
+            var element2 = document.getElementById('7');
+            element2.style.display = 'none'
+            break;
+        case '7':
+            var element = document.getElementById('1');
+            element.style.display = 'none';
+            var element1 = document.getElementById('2');
+            element1.style.display = 'none';
+            var element2 = document.getElementById('3');
+            element2.style.display = 'none';
+            var element2 = document.getElementById('4');
+            element2.style.display = 'none';
+            var element2 = document.getElementById('5');
+            element2.style.display = 'none'
+            var element2 = document.getElementById('6');
+            element2.style.display = 'none'
+            var element2 = document.getElementById('7');
+            element2.style.display = 'block'
             break;
     }
 }
