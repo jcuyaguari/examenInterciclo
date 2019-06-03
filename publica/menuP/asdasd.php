e1cd406f47d036e26e5ea8c65c827f8183528bf1
RIcardoVinicioJaraJara <49212955+RIcardoVinicioJaraJara@users.noreply.github.com>
2019-06-03 00:05:19 -0500

    diseño de las compras

--- publica/menuP/asdasd.php
+++ publica/menuP/asdasd.php
@@ -21,7 +21,7 @@
                 <?php echo '<img style="width: 383px; height: 245px;" src="data:image/jpg;base64,' . base64_encode($row['pro_imagen']) . '">'; ?>
                 <h1 style="border: double  black 10px;"> <?php echo $row['pro_nombre']; ?></h1>
                 <b style="border: dotted  black 2px;"> <?php echo "$ " . $row['pro_precio'] ?></b> <br><br>
-                <i style="border: solid  black 2px;"> <?php echo "Unidades Disponibles" . $row['pro_stock']; ?></i>
+                <i style="border: solid  black 2px;"> <?php echo "Unidades Disponibles" . $row['pro_stock']; ?></i><br><br>
                 <label class="switch">
                     <input type="checkbox">
                     <span class="slider"></span>
@@ -30,9 +30,12 @@
             </fieldset>
         <?php
     }
+    ?>
+        <button name="button"  style="position: relative; margin-top: 20px; margin-left: 25%; height: 100px; width: 400px; float: left;">COMPRAR PRODUCTOS</button>
+    <?php
 } else {
     echo "<tr>";
-    echo " <td colspan='7'> No existen correos </td> ";
+    echo " <td colspan='7'> NO EXISTEN PRODUCTOS </td> ";
     echo "</tr>";
 }
 ?>
