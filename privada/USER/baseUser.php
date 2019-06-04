612f66b3235cebf975bedfa91cbe3a15aaea1440
Andres Loja <aloja619@gmail.com>
2019-06-03 20:11:18 -0500

    fecha modificacion

--- privada/USER/baseUser.php
+++ privada/USER/baseUser.php
@@ -11,8 +11,10 @@
     $opc = $_GET['opc'];
     switch ($opc) {
         case 'reactivar':
+            date_default_timezone_set("America/Guayaquil");
+            $fechaM = date('Y-m-d H:i:s', time());
             $codigo  = $_GET['codigo'];
-            $sql = "UPDATE usuario SET usu_eliminado='N' WHERE usu_codigo='$codigo'";
+            $sql = "UPDATE usuario SET usu_eliminado='N', usu_fecha_modificacion='$fechaM' WHERE usu_codigo='$codigo'";
             if ($conn->query($sql) === TRUE) {
                 echo "TRUE";
             } else {
@@ -21,8 +23,10 @@
             }
             break;
         case 'eliminar':
+            date_default_timezone_set("America/Guayaquil");
+            $fechaM = date('Y-m-d H:i:s', time());
             $codigo  = $_GET['codigo'];
-            $sql = "UPDATE usuario SET usu_eliminado='S' WHERE usu_codigo='$codigo'";
+            $sql = "UPDATE usuario SET usu_eliminado='S', usu_fecha_modificacion='$fechaM' WHERE usu_codigo='$codigo'";
             if ($conn->query($sql) === TRUE) {
                 echo "TRUE";
             } else {
@@ -35,7 +39,7 @@
             $fechaM = date('Y-m-d H:i:s', time());
             $codigo = $_GET["codigo"];
             $contrasenaN = $_GET["contrasenaN"];
-            echo("$codigo");
+            echo ("$codigo");
             $sql = "UPDATE usuario SET usu_password= MD5('$contrasenaN'), usu_fecha_modificacion='$fechaM' WHERE usu_codigo='$codigo'";
             if ($conn->query($sql) === TRUE) {
                 echo "TRUE";
